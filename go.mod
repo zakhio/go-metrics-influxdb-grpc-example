254e86446f5ca428507b4952012997cf983625ab
Andrey Zakharov <avzakharoff@gmail.com>
2020-07-22 21:50:37 +0200

    * Updates go-metrics-influxdb library * Adds histogram example

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	github.com/golang/protobuf v1.4.2
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
-	github.com/zakhio/go-metrics-influxdb v0.3.0
+	github.com/zakhio/go-metrics-influxdb v0.3.1
 	google.golang.org/grpc v1.30.0
 	google.golang.org/protobuf v1.25.0
 	gopkg.in/yaml.v2 v2.2.5
--- server/handler/echoService.go
+++ server/handler/echoService.go
@@ -10,7 +10,10 @@ import (
 )
 
 var (
-	echoCounter = metrics.GetOrRegisterCounter("echo", nil)
+	// A uniform sample using Vitter's Algorithm R.
+	// <http://www.cs.umd.edu/~samir/498/vitter.pdf>
+	messageLengthHistogram = metrics.GetOrRegisterHistogram("message_length", nil, metrics.NewUniformSample(50))
+	messageCounter         = metrics.GetOrRegisterCounter("message", nil)
 )
 
 type echoServer struct {
@@ -20,7 +23,9 @@ type echoServer struct {
 
 //Echo sends back message from the request and its number
 func (s *echoServer) Echo(_ context.Context, req *proto.EchoRequest) (*proto.EchoResponse, error) {
-	echoCounter.Inc(1)
+	messageLengthHistogram.Update(int64(len(req.Message)))
+	messageCounter.Inc(1)
+
 	count := atomic.AddUint64(&s.counter, 1)
 
 	return &proto.EchoResponse{Message: fmt.Sprintf("Message #%d: %v", count, req.Message)}, nil
